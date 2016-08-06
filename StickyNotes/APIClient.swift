e56373abc796c0ab0ce9cf16c28117e4d5c26a41
Hiroki Kumamoto <kumabook@live.jp>
2016-08-06 16:07:21 +0900

    Implement Fetch Stickies API

--- StickyNotes/APIClient.swift
+++ StickyNotes/APIClient.swift
@@ -17,14 +17,18 @@ let baseUrl = "https://stickynotes-backend.herokuapp.com"
 protocol StickyNoteRequestType: RequestType {
 }
 
-extension StickyNoteRequestType where Response: Decodable {
+extension StickyNoteRequestType {
     var baseURL: NSURL {
         return NSURL(string: baseUrl)!
     }
+}
+
+extension StickyNoteRequestType where Response: Decodable {
     func responseFromObject(object: AnyObject, URLResponse: NSHTTPURLResponse) throws -> Response {
         return try decodeValue(object)
     }
 }
+
 
 struct AccessTokenRequest: StickyNoteRequestType {
     typealias Response = AccessToken
@@ -53,3 +57,60 @@ struct AccessToken: Decodable {
     }
 }
 
+struct StickiesRequest: StickyNoteRequestType {
+    typealias Response = StickiesResponse
+    var newerThan: NSDate
+    var path: String { return "/api/v1/stickies.json" }
+    var method: HTTPMethod { return .GET }
+    var parameters: AnyObject? {
+        let dateFormatter = NSDateFormatter()
+        let enUSPosixLocale = NSLocale(localeIdentifier: "en_US_POSIX")
+        dateFormatter.locale = enUSPosixLocale
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZZ"
+        return ["newer_than": dateFormatter.stringFromDate(newerThan)]
+    }
+}
+
+struct StickiesResponse: Decodable {
+    var value: [Sticky]
+    static func decode(e: Extractor) throws -> StickiesResponse {
+        return StickiesResponse(value: try e.array(KeyPath.empty))
+    }
+}
+
+struct Sticky: Decodable {
+    var id: String
+    var uuid: String
+    var page_id: Int
+    var left: Int
+    var top: Int
+    var width: Int
+    var height: Int
+    var content: String
+    var color: String
+    var state: Int
+    var createdAt: NSDate
+    var updatedAt: NSDate
+    var userId: Int
+
+    static func decode(e: Extractor) throws -> Sticky {
+        let formatter = NSDateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+        let posix = NSLocale(localeIdentifier: "en_US_POSIX")
+        formatter.locale = posix
+        return try Sticky(
+            id: e <| "id",
+            uuid: e <| "uuid",
+            page_id: e <| "page_id",
+            left: e <| "left",
+            top:  e <| "top",
+            width: e <| "width",
+            height: e <| "height",
+            content: e <| "content",
+            color: e <| "color",
+            state: e <| "state",
+            createdAt: formatter.dateFromString(e <| "created_at")!,
+            updatedAt: formatter.dateFromString(e <| "updated_at")!,
+            userId: e <| "user_id")
+    }
+}
