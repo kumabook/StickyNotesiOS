284ab42e7b462fecc24d94f553a3771b24a73820
Hiroki Kumamoto <kumabook@live.jp>
2017-05-05 00:27:38 +0900

    Add MBProgressHUDExtension

---
+++ StickyNotes/MBProgressHUDExtension.swift
@@ -0,0 +1,39 @@
+//
+//  MBProgressHUDExtension.swift
+//  StickyNotes
+//
+//  Created by Hiroki Kumamoto on 2017/05/04.
+//  Copyright © 2017 kumabook. All rights reserved.
+//
+
+import Foundation
+import MBProgressHUD
+
+extension MBProgressHUD {
+    fileprivate class func createCompletedHUD(_ view: UIView) -> MBProgressHUD {
+        let hud = MBProgressHUD(view: view)
+        hud.customView = UIImageView(image:UIImage(named:"checkmark"))
+        hud.mode = MBProgressHUDMode.customView
+        hud.label.text = "Completed".localize()
+        return hud
+    }
+    
+    class func showCompletedHUDForView(_ view: UIView, animated: Bool, duration: TimeInterval, after: @escaping () -> Void) -> MBProgressHUD {
+        let hud = MBProgressHUD.createCompletedHUD(view)
+        view.addSubview(hud)
+        hud.show(true, duration: duration, after: {
+            hud.removeFromSuperview()
+            after()
+        })
+        return hud
+    }
+    
+    func show(_ animated:Bool, duration:TimeInterval, after:@escaping () -> Void) {
+        show(animated: true)
+        let startTime = DispatchTime.now() + Double(Int64(duration * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC)
+        DispatchQueue.main.asyncAfter(deadline: startTime) { () -> Void in
+            self.hide(animated: true)
+            after()
+        }
+    }
+}
